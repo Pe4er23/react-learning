5bccd69c4f5688f2adeeb2530cc0fe1bd97cc3d2
Pe4er23 <pe4enka3006@gmail.com>
2026-04-06 04:13:32 +0300

    add header and heading components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,11 +12,8 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <Header />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
